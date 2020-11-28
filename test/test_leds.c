42a56a76674dbde14c9b0536e7bbe47a76d8ddd1
Nahuel Espinosa <nahue.espinosa@gmail.com>
2020-11-28 18:51:22 -0300

    Add turn on all leds test

--- test/test_leds.c
+++ test/test_leds.c
@@ -16,26 +16,31 @@ void test_LedsOffAfterCreate(void) {
     TEST_ASSERT_EQUAL_HEX16(0, ledsVirtuales);
 }
 
-void test_SingleLedTurnOn(void) {
+void test_TurnOnSingleLed(void) {
     Leds_TurnOn(4);
     TEST_ASSERT_EQUAL_HEX16(0x8, ledsVirtuales);
 }
 
-void test_SingleLedTurnOff(void) {
+void test_TurnOffSingleLed(void) {
     Leds_TurnOn(1);
     Leds_TurnOff(1);
     TEST_ASSERT_EQUAL_HEX16(0, ledsVirtuales);
 }
 
-void test_MultipleLedTurnOn(void) {
+void test_TurnOnMultipleLeds(void) {
     Leds_TurnOn(7);
     Leds_TurnOn(10);
     TEST_ASSERT_EQUAL_HEX16(0x0240, ledsVirtuales);
 }
 
-void test_MultipleLedTurnOff(void) {
+void test_TurnOffMultipleLeds(void) {
     Leds_TurnAllOn();
     Leds_TurnOff(6);
     Leds_TurnOff(2);
     TEST_ASSERT_EQUAL_HEX16(0xFFDD, ledsVirtuales);
+}
+
+void test_TurnOnAllLeds() {
+    Leds_TurnAllOn();
+    TEST_ASSERT_EQUAL_HEX16(0xFFFF, ledsVirtuales);
 }
